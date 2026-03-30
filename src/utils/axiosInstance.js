deb052a50b88ebbf06439853e82a3cea644d3fab
nat-2135 <natchathra2135@gmail.com>
2026-03-30 21:15:00 +0530

    Configure live Render URL as the default API endpoint

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080'
+  baseURL: import.meta.env.VITE_API_URL || 'https://digital-classroom-engagement-scoring.onrender.com'
 });
 
 api.interceptors.request.use(config => {
